4af3b2986a4348797e798e86ad87ab35246b85d3
cse <cse@cse-lab.localdomain>
2012-12-16 06:02:51 -0800

    lots of comments

--- extent_client.cc
+++ extent_client.cc
@@ -19,132 +19,298 @@ extent_client::extent_client(std::string dst)
   VERIFY(pthread_mutex_init(&cache_mutex_, NULL) == 0);
 }
 
-extent_protocol::status extent_client::get_cache(extent_protocol::extentid_t eid, std::string &buf){
+//extent_protocol::status extent_client::get_cache(extent_protocol::extentid_t eid, std::string &buf){
+//	extent_protocol::status ret = extent_protocol::OK;
+//	pthread_mutex_lock(&cache_mutex_);
+//
+//	std::map<extent_protocol::extentid_t, extent_protocol::attr>::iterator it_a;
+//	std::map<extent_protocol::extentid_t, std::string>::iterator it;
+//	it_a = attr_cache_.find(eid);
+//	if (it_a == attr_cache_.end()){
+//		extent_protocol::attr attr;
+//		ret = cl->call(extent_protocol::getattr, eid, attr);
+//		if (ret == extent_protocol::OK){
+//			attr_cache_[eid] = attr;
+//		}else{
+//			tprintf("ret != ok in %s\n", __FUNCTION__);
+//			goto release;
+//		}
+//	}
+//
+//	it = data_cache_.find(eid);
+//	if (it == data_cache_.end()){
+//		ret = cl->call(extent_protocol::get, eid, buf);
+//		if (ret == extent_protocol::OK){
+//			data_cache_[eid] = buf;
+//			cache_states_[eid].dirty_ = false;
+//		}else{
+//			tprintf("ret != ok in %s\n", __FUNCTION__);
+//		}
+//	}else{
+//		buf = data_cache_[eid];
+//	}	
+//	it_a->second.atime = static_cast<unsigned int>(time(NULL));
+//
+//release:
+//	pthread_mutex_unlock(&cache_mutex_);
+//	return ret;
+//}
+//
+//extent_protocol::status extent_client::get_attr_cache(extent_protocol::extentid_t eid, extent_protocol::attr &attr){
+//	extent_protocol::status ret = extent_protocol::OK;
+//	pthread_mutex_lock(&cache_mutex_);
+//	std::map<extent_protocol::extentid_t, extent_protocol::attr>::iterator it;
+//	it = attr_cache_.find(eid);
+//	if (it == attr_cache_.end()){
+//		ret = cl->call(extent_protocol::getattr, eid, attr);
+//		if (ret == extent_protocol::OK){
+//			attr_cache_[eid] = attr;
+//		}else{
+//			tprintf("ret != ok in %s\n", __FUNCTION__);
+//		}
+//	}else{
+//		attr = attr_cache_[eid];
+//		tprintf("attr from cache %llu\n", eid);
+//	}
+//	tprintf("get attr cache %llu\n", eid);
+//	pthread_mutex_unlock(&cache_mutex_);
+//	return ret;
+//}
+//
+//extent_protocol::status extent_client::put_cache(extent_protocol::extentid_t eid, std::string buf){
+//	extent_protocol::status ret = extent_protocol::OK;
+//	pthread_mutex_lock(&cache_mutex_);
+//
+//	std::map<extent_protocol::extentid_t, std::string>::iterator it;
+//	std::map<extent_protocol::extentid_t, extent_protocol::attr>::iterator it_a;
+//
+//	it_a = attr_cache_.find(eid);
+//	if (it_a == attr_cache_.end()){
+//		extent_protocol::attr attr;
+//		ret = cl->call(extent_protocol::getattr, eid, attr);
+//		if (ret == extent_protocol::OK){
+//			attr_cache_[eid] = attr;
+//		}else{
+//			tprintf("ret != ok in %s\n", __FUNCTION__);
+//			goto release;
+//		}
+//	}
+//
+//	it = data_cache_.find(eid);
+//	if (it == data_cache_.end()){
+//		int r = 0;
+//		ret = cl->call(extent_protocol::put, eid, buf, r);
+//		if (ret == extent_protocol::OK){
+//			data_cache_[eid] = buf;
+//			cache_states_[eid].dirty_ = false;
+//			attr_cache_[eid].mtime = static_cast<unsigned int>(time(NULL));
+//		}else{
+//			tprintf("ret != ok in %s\n", __FUNCTION__);
+//		}
+//	}else{
+//		data_cache_[eid] = buf;
+//		cache_states_[eid].dirty_ = true;
+//		it_a->second.mtime = static_cast<unsigned int>(time(NULL));
+//		it_a->second.size = buf.size();
+//	}
+//
+//release:
+//	pthread_mutex_unlock(&cache_mutex_);
+//	return ret;
+//}
+//
+//extent_protocol::status extent_client::put_cache_new(extent_protocol::extentid_t eid, std::string buf, int &rid){
+//	extent_protocol::status ret = extent_protocol::OK;
+//	pthread_mutex_lock(&cache_mutex_);
+//	ret = cl->call(extent_protocol::put, eid, buf, rid);
+//	if (ret == extent_protocol::OK){
+//		extent_protocol::extentid_t new_id = static_cast<extent_protocol::extentid_t>(rid);
+//		data_cache_[new_id]= buf;
+//		cache_states_[new_id].dirty_ = false;
+//		unsigned int t = static_cast<unsigned int>(time(NULL));
+//		attr_cache_[new_id].atime = t;
+//		attr_cache_[new_id].ctime = t;
+//		attr_cache_[new_id].mtime = t;
+//		attr_cache_[new_id].size = buf.size();
+//	}else{
+//		tprintf("ret != ok in %s\n", __FUNCTION__);
+//	}
+//	tprintf("put new %llu\n", eid);
+//	pthread_mutex_unlock(&cache_mutex_);
+//	return ret;
+//}
+//
+//extent_protocol::status extent_client::remove_cache(extent_protocol::extentid_t eid){
+//	extent_protocol::status ret = extent_protocol::OK;
+//	pthread_mutex_lock(&cache_mutex_);
+//	int r = 0;
+//	ret = cl->call(extent_protocol::remove, eid, r);
+//	if (ret == extent_protocol::OK){
+//		if (data_cache_.find(eid) != data_cache_.end()){
+//			data_cache_.erase(eid);
+//		}
+//		if (attr_cache_.find(eid) != attr_cache_.end()){
+//			attr_cache_.erase(eid);
+//		}
+//		if (cache_states_.find(eid) != cache_states_.end()){
+//			cache_states_.erase(eid);
+//		}
+//	}else{
+//		tprintf("ret != ok in %s\n", __FUNCTION__);
+//	}
+//	pthread_mutex_unlock(&cache_mutex_);
+//	return ret;
+//}
+
+//new private cache method
+void extent_client::update_cache(extent_protocol::extentid_t eid, std::string buf){
+	data_cache_[eid] = buf;
+
+	unsigned int t = static_cast<unsigned int>(time(NULL));
+	attr_cache_[eid].atime = t;
+	attr_cache_[eid].mtime = t;
+	attr_cache_[eid].ctime = t;
+	attr_cache_[eid].size = buf.size();
+
+	cache_states_[eid].update();	
+}
+
+bool extent_client::access_cache(extent_protocol::extentid_t eid, std::string &buf){
+	if (cache_states_[eid].is_valid()){
+		buf = data_cache_[eid];
+		attr_cache_[eid].atime = static_cast<unsigned int>(time(NULL));
+		return true;
+	}else{
+		return false;
+	}
+}
+
+
+//in case that data is not in cache
+//return false when that happens
+bool extent_client::modify_cache(extent_protocol::extentid_t eid, std::string buf){
+	if (cache_states_[eid].is_valid()){
+		data_cache_[eid] = buf;
+		unsigned int t = static_cast<unsigned int>(time(NULL));
+		attr_cache_[eid].atime = t;
+		attr_cache_[eid].mtime = t;
+		
+		cache_states_[eid].modify();
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool extent_client::access_attr_cache(extent_protocol::extentid_t eid, extent_protocol::attr &attr){
+	if (cache_states_[eid].is_attr_valid()){
+		attr = attr_cache_[eid];
+		return true;
+	}else{
+		return false;
+	}
+}
+
+void extent_client::update_attr_cache(extent_protocol::extentid_t eid, extent_protocol::attr attr){
+	attr_cache_[eid] = attr;
+
+	cache_states_[eid].update_attr();
+}
+
+
+void extent_client::flush_cache(extent_protocol::extentid_t eid){
+
+}
+
+//in case that data is not in cache
+//return false when that happens
+bool extent_client::delete_cache(extent_protocol::extentid_t eid){
+	if (cache_states_[eid].is_valid()){
+		unsigned int t = static_cast<unsigned int>(time(NULL));
+		attr_cache_[eid].atime = t;
+		attr_cache_[eid].mtime = t;
+		
+		cache_states_[eid].del();
+		return true;
+	}else{
+		return false;
+	}
+}
+//public interface
+
+extent_protocol::status extent_client::get(extent_protocol::extentid_t eid, std::string &buf){
 	extent_protocol::status ret = extent_protocol::OK;
 	pthread_mutex_lock(&cache_mutex_);
-	std::map<extent_protocol::extentid_t, std::string>::iterator it;
-	it = data_cache_.find(eid);
-	if (it == data_cache_.end()){
+	if (access_cache(eid, buf)){
+		goto release;
+	}else{
 		ret = cl->call(extent_protocol::get, eid, buf);
 		if (ret == extent_protocol::OK){
-			data_cache_[eid] = buf;
-			cache_states_[eid].dirty_ = false;
-		}else{
-			tprintf("ret != ok in %s\n", __FUNCTION__);
+			update_cache(eid, buf);
 		}
-	}else{
-		buf = data_cache_[eid];
-	}	
-	attr_cache_[eid].atime = static_cast<unsigned int>(time(NULL));
+		goto release;
+	}
+release:
 	pthread_mutex_unlock(&cache_mutex_);
 	return ret;
 }
 
-extent_protocol::status extent_client::get_attr_cache(extent_protocol::extentid_t eid, extent_protocol::attr &attr){
+
+extent_protocol::status extent_client::getattr(extent_protocol::extentid_t eid, extent_protocol::attr &attr){
 	extent_protocol::status ret = extent_protocol::OK;
 	pthread_mutex_lock(&cache_mutex_);
-	std::map<extent_protocol::extentid_t, extent_protocol::attr>::iterator it;
-	it = attr_cache_.find(eid);
-	if (it == attr_cache_.end()){
+	if (access_attr_cache(eid, attr)){
+		goto release;
+	}else{
 		ret = cl->call(extent_protocol::getattr, eid, attr);
 		if (ret == extent_protocol::OK){
-			attr_cache_[eid] = attr;
-		}else{
-			tprintf("ret != ok in %s\n", __FUNCTION__);
+			update_attr_cache(eid, attr);
 		}
-	}else{
-		attr = attr_cache_[eid];
-		tprintf("attr from cache %llu\n", eid);
+		goto release;
 	}
-	tprintf("get attr cache %llu\n", eid);
+release:
 	pthread_mutex_unlock(&cache_mutex_);
 	return ret;
 }
 
-extent_protocol::status extent_client::put_cache(extent_protocol::extentid_t eid, std::string buf){
+
+extent_protocol::status extent_client::put(extent_protocol::extentid_t eid, std::string buf){
 	extent_protocol::status ret = extent_protocol::OK;
 	pthread_mutex_lock(&cache_mutex_);
-	std::map<extent_protocol::extentid_t, std::string>::iterator it;
-	it = data_cache_.find(eid);
-	if (it == data_cache_.end()){
-		int r = 0;
-		ret = cl->call(extent_protocol::put, eid, buf, r);
-		if (ret == extent_protocol::OK){
-			data_cache_[eid] = buf;
-			cache_states_[eid].dirty_ = false;
-			attr_cache_[eid].mtime = static_cast<unsigned int>(time(NULL));
-		}else{
-			tprintf("ret != ok in %s\n", __FUNCTION__);
-		}
+	if (modify_cache(eid, buf)){
+		goto release;
 	}else{
-		data_cache_[eid] = buf;
-		cache_states_[eid].dirty_ = true;
-		attr_cache_[eid].mtime = static_cast<unsigned int>(time(NULL));
-		attr_cache_[eid].size = buf.size();
+		std::string in_buf;
+		ret = cl->call(extent_protocol::get, eid, in_buf);
+		if (ret == extent_protocol::OK){
+			update_cache(eid, in_buf);
+			VERIFY(modify_cache(eid, buf));
+		}
+		goto release;
 	}
+release:
 	pthread_mutex_unlock(&cache_mutex_);
 	return ret;
 }
 
-extent_protocol::status extent_client::put_cache_new(extent_protocol::extentid_t eid, std::string buf, int &rid){
+extent_protocol::status extent_client::put(extent_protocol::extentid_t eid, std::string buf, int &rid){
 	extent_protocol::status ret = extent_protocol::OK;
 	pthread_mutex_lock(&cache_mutex_);
 	ret = cl->call(extent_protocol::put, eid, buf, rid);
 	if (ret == extent_protocol::OK){
-		data_cache_[static_cast<extent_protocol::extentid_t>(rid)] = buf;
-		cache_states_[eid].dirty_ = false;
+		update_cache(static_cast<extent_protocol::extentid_t>(rid), buf);
+		goto release;
 	}else{
-		tprintf("ret != ok in %s\n", __FUNCTION__);
+		goto release;
 	}
-	tprintf("put new %llu\n", eid);
+release:
 	pthread_mutex_unlock(&cache_mutex_);
 	return ret;
-}
-
-extent_protocol::status extent_client::remove_cache(extent_protocol::extentid_t eid){
-	extent_protocol::status ret = extent_protocol::OK;
-	pthread_mutex_lock(&cache_mutex_);
-	int r = 0;
-	ret = cl->call(extent_protocol::remove, eid, r);
-	if (ret == extent_protocol::OK){
-		if (data_cache_.find(eid) != data_cache_.end()){
-			data_cache_.erase(eid);
-		}
-		if (attr_cache_.find(eid) != attr_cache_.end()){
-			attr_cache_.erase(eid);
-		}
-		if (cache_states_.find(eid) != cache_states_.end()){
-			cache_states_.erase(eid);
-		}
-	}else{
-		tprintf("ret != ok in %s\n", __FUNCTION__);
-	}
-	pthread_mutex_unlock(&cache_mutex_);
-	return ret;
-}
-
-
-//public interface
-
-extent_protocol::status extent_client::get(extent_protocol::extentid_t eid, std::string &buf){
-	return get_cache(eid, buf);
-}
-
-extent_protocol::status extent_client::getattr(extent_protocol::extentid_t eid, extent_protocol::attr &attr){
-	return get_attr_cache(eid, attr);
-}
-
-extent_protocol::status extent_client::put(extent_protocol::extentid_t eid, std::string buf){
-	return put_cache(eid, buf);
-}
-
-extent_protocol::status extent_client::put(extent_protocol::extentid_t eid, std::string buf, int &rid){
-	return put_cache_new(eid, buf, rid);
 }
 
 
 extent_protocol::status extent_client::remove(extent_protocol::extentid_t eid){
-	return remove_cache(eid);
+	return 0;
 }
 
 
--- extent_client.h
+++ extent_client.h
@@ -12,20 +12,62 @@
 class extent_client {
 private:
 	rpcc *cl;
-	struct cache_state_t{
+	class cache_state_t{
+	public:
+		cache_state_t(){
+			valid_ = false;
+			dirty_ = false;
+			deleted_ = false;
+			attr_valid_ = false;
+		}
+
+		void update(){
+			valid_ = true;
+			dirty_ = false;
+			deleted_ = false;
+			attr_valid_ = true;
+		}
+
+		void update_attr(){
+			attr_valid_ = true;
+		}
+
+		bool is_valid(){
+			return valid_;
+		}
+		bool is_attr_valid(){
+			return attr_valid_;
+		}
+		void modify(){
+			dirty_ = true;
+		}
+
+		void del(){
+			deleted_ = true;
+		}
+		bool valid_;
 		bool dirty_;
+		bool deleted_;
+		bool attr_valid_;
 	};
 	pthread_mutex_t	cache_mutex_;
 	std::map<extent_protocol::extentid_t, cache_state_t> cache_states_;
 	std::map<extent_protocol::extentid_t, extent_protocol::attr> attr_cache_;
 	std::map<extent_protocol::extentid_t, std::string> data_cache_;
 
+	void update_cache(extent_protocol::extentid_t eid, std::string buf);
+	bool access_cache(extent_protocol::extentid_t eid, std::string &buf);
+	void update_attr_cache(extent_protocol::extentid_t eid, extent_protocol::attr attr);
+	bool access_attr_cache(extent_protocol::extentid_t eid, extent_protocol::attr &attr);
+	bool modify_cache(extent_protocol::extentid_t eid, std::string buf);
+	void flush_cache(extent_protocol::extentid_t eid);
+	bool delete_cache(extent_protocol::extentid_t eid);
 
-	extent_protocol::status get_cache(extent_protocol::extentid_t eid, std::string &buf);
-	extent_protocol::status put_cache(extent_protocol::extentid_t eid, std::string buf);
-	extent_protocol::status put_cache_new(extent_protocol::extentid_t eid, std::string buf, int &rid);
-	extent_protocol::status get_attr_cache(extent_protocol::extentid_t eid, extent_protocol::attr &attr);
-	extent_protocol::status remove_cache(extent_protocol::extentid_t eid);
+//	extent_protocol::status get_cache(extent_protocol::extentid_t eid, std::string &buf);
+//	extent_protocol::status put_cache(extent_protocol::extentid_t eid, std::string buf);
+//	extent_protocol::status put_cache_new(extent_protocol::extentid_t eid, std::string buf, int &rid);
+//	extent_protocol::status get_attr_cache(extent_protocol::extentid_t eid, extent_protocol::attr &attr);
+//	extent_protocol::status remove_cache(extent_protocol::extentid_t eid);
 
 public:
 	extent_client(std::string dst);
